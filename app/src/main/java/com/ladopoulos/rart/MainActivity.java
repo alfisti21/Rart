6ac31f90edc00804eab55acc7c0fb89a2c0a70be
Angelos Ladopoulos <alfisti21@gmail.com>
2020-03-06 10:13:08 +0100

    -Added image view with URL picture using Picasso library. -Added text view (empty for now) to display painting information/.

--- app/src/main/java/com/ladopoulos/rart/MainActivity.java
+++ app/src/main/java/com/ladopoulos/rart/MainActivity.java
@@ -1,8 +1,9 @@
 package com.ladopoulos.rart;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final ImageView favs = findViewById(R.id.painting);
+        final String paintingImage = "https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg";
+
+        Picasso.get().load(paintingImage)
+                .noFade().into(favs);
     }
 }
